f43a76d0fd300d3225b31d9f3b816d6558d80d0d
KalininGit <109882612+KaliNinB@users.noreply.github.com>
2023-10-02 00:01:37 +0300

    Change environment variables for production mode; add enableProdMode to main.ts and configure angular.json for production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:5001/odata/'
+  apiUrl: 'https://moda-store-api.azurewebsites.net/odata/',
+  stripePublicKey: 'pk_test_51N2dxoAoGAOX0ZrldcpNA9P3tDlCWnSlv1S1dHfEcuqRxdJ6d4td8X0bINRoEOAF1vq99FvWIcQaRqj6NYSA5iZ200jX0tZYRh'
 };
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import {VERSION as CDK_VERSION} from '@angular/cdk';
-import {VERSION as MAT_VERSION} from '@angular/material/core';
 import {enableProdMode} from '@angular/core';
+import { environment } from './environments/environment';
 
 
-/* eslint-disable no-console */
-console.info('Angular CDK version', CDK_VERSION.full);
-console.info('Angular Material version', MAT_VERSION.full);
+if (environment.production) {
+  enableProdMode();
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
