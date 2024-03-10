f1264b605fda3fef7037d41e334195c5556ddae2
Vlad Kalinin <109882612+vladKalinin007@users.noreply.github.com>
2024-03-10 18:15:19 +0200

    Update environment.enum.ts

--- src/app/core/config/environment.enum.ts
+++ src/app/core/config/environment.enum.ts
@@ -8,6 +8,6 @@ export enum DevEnvironment {
 
 export enum ProdEnvironment {
     PRODUCTION = 'true',
-    API_URL = 'https://modastoreapi.up.railway.app/api/',
+    API_URL = 'https://modaapi.onrender.com/api/',
     STRIPE_PUBLIC_KEY = 'pk_test_51H4W1pK0zW2cLxk4Y6q3j6v4mQmZq5Y3fZ1n1q6ZB1Qg1Iz5Z6dXjX5d7m0b7aK7c6J3j2v9Q8w6MlHm4Y1UqJ0z00Hv0aKU8Q'
 }
